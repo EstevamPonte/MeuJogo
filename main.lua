ccc7a215815901fd14ae146802b0c314e6168165
EstevamPonte <tecoponte@gmail.com>
2019-02-16 15:30:12 -0200

    primeiro commit do jogo

---
+++ main.lua
@@ -0,0 +1,48 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+local background = display.newImageRect("oceano.png", 800, 1400 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local boneco = display.newImageRect("bonequinho.png",80, 80)
+boneco.x = display.contentCenterX
+boneco.y = display.contentCenterY-200
+
+local setaesquerda = display.newImageRect("seta-1.png",60, 60)
+setaesquerda.x = display.contentCenterX - 90
+setaesquerda.y = display.contentCenterY+ 220
+
+local setadireita = display.newImageRect("seta-2.png",60, 60)
+setadireita.x = display.contentCenterX + 90
+setadireita.y = display.contentCenterY+ 220
+
+
+
+moverx = 0 -- variavel usada para mover o boneco ao longo do eixo x
+velocidade = 6 -- Set Walking velocidade
+
+local function movePlayer(event)
+    boneco.x = boneco.x + moverx
+end
+Runtime:addEventListener("enterFrame", movePlayer)
+ 
+function playerVelocity(event)
+    if (event.phase == "began") then
+        if event.x  >= display.contentCenterX then
+            moverx = velocidade
+            boneco.xScale = 1
+    elseif event.x <= display.contentCenterX then
+        moverx = -velocidade
+        boneco.xScale = -1
+end
+elseif (event.phase == "ended") then
+moverx = 0
+end
+end
+background:addEventListener("touch", playerVelocity)
