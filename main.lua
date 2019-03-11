acc724be451b6082a3acdced0016e9bfe811207c
EstevamPonte <tecoponte@gmail.com>
2019-03-11 16:23:32 -0300

    adicionando a limitaçao do personagem

--- main.lua
+++ main.lua
@@ -5,6 +5,20 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+local physics = require( "physics" )
+physics.start()
+
+--mostra a fisica
+physics.setDrawMode("hybrid")
+
+local paredeDireita = display.newRect( 316, display.contentCenterY, 10, 500 )
+physics.addBody( paredeDireita, "static", { friction = 1} )
+
+local paredeEsquerda = display.newRect( 1, display.contentCenterY, 10, 500 )
+physics.addBody( paredeEsquerda, "static", {friction = 1} )
+
+local plataforma = display.newRect( display.contentCenterX, 80, display.contentWidth, 10 )
+physics.addBody( plataforma, "static",{ friction = 1, })
 
 local background = display.newImageRect("oceano.png", 800, 1400 )
 background.x = display.contentCenterX
@@ -13,6 +27,13 @@ background.y = display.contentCenterY
 local boneco = display.newImageRect("bonequinho.png",80, 80)
 boneco.x = display.contentCenterX
 boneco.y = display.contentCenterY-200
+physics.addBody( boneco, "dynamic",  {  friction = 1, filter = collisionFilter})
+
+
+
+local collisionFilter = { groupIndex = 2}
+
+
 
 local setaesquerda = display.newImageRect("seta-1.png",60, 60)
 setaesquerda.x = display.contentCenterX - 90
@@ -21,8 +42,6 @@ setaesquerda.y = display.contentCenterY+ 220
 local setadireita = display.newImageRect("seta-2.png",60, 60)
 setadireita.x = display.contentCenterX + 90
 setadireita.y = display.contentCenterY+ 220
-
-
 
 moverx = 0 -- variavel usada para mover o boneco ao longo do eixo x
 velocidade = 6 -- Set Walking velocidade
